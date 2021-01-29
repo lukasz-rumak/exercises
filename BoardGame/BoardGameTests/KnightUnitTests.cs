e7822677035a6ceaea3e965c3162542c8849d6b8
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2021-01-29 10:24:38 +0100

    Board Game

---
+++ BoardGame/BoardGameTests/KnightUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using BoardGame.Interfaces;
+using BoardGame.Managers;
+using BoardGame.Models;
+using Xunit;
+
+namespace BoardGameTests
+{
+    public class KnightUnitTests
+    {
+        private IPiece _knight;
+
+        public KnightUnitTests()
+        {
+            _knight = new Knight(0, Piece.Knight);
+        }
+
+        [Theory]
+        [InlineData(Direction.NorthEast, Direction.SouthEast)]
+        [InlineData(Direction.SouthEast, Direction.SouthWest)]
+        [InlineData(Direction.SouthWest, Direction.NorthWest)]
+        [InlineData(Direction.NorthWest, Direction.NorthEast)]
+        [InlineData(Direction.None, Direction.None)]
+        public void ReturnExpectedVersusActualForChangeDirectionToRight(Direction input, Direction expectedResult)
+        {
+            _knight.Position.Direction = input;
+            _knight.ChangeDirectionToRight();
+            Assert.Equal(expectedResult, _knight.Position.Direction);
+        }
+
+        [Theory]
+        [InlineData(Direction.NorthWest, Direction.SouthWest)]
+        [InlineData(Direction.SouthWest, Direction.SouthEast)]
+        [InlineData(Direction.SouthEast, Direction.NorthEast)]
+        [InlineData(Direction.NorthEast, Direction.NorthWest)]
+        [InlineData(Direction.None, Direction.None)]
+        public void ReturnExpectedVersusActualForChangeDirectionToLeft(Direction input, Direction expectedResult)
+        {
+            _knight.Position.Direction = input;
+            _knight.ChangeDirectionToLeft();
+            Assert.Equal(expectedResult, _knight.Position.Direction);
+        }
+    }
+}
