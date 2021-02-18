0f7a4611c678d2f5347ed338673e3d955423870e
Lukasz Rumak <lukasz.rumak@newvoicemedia.com>
2021-02-18 16:50:25 +0100

    Board Game

---
+++ BoardGame/BoardGameApi/Controllers/BoardGameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGameApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardGameController : ControllerBase
+    {
+        private readonly IEnumerable<string> _something;
+
+        public BoardGameController()
+        {
+            _something = new List<string> { "something 1", "something 2", "something 3" };
+        }
+
+        [HttpGet("doGet")]
+        public string Get()
+        {
+            var strBuilder = new StringBuilder();
+            foreach (var val in _something)
+            {
+                strBuilder.Append(val);
+                strBuilder.Append(", ");
+            }
+            return strBuilder.ToString();
+        }
+        
+        [HttpPost("doPost")]
+        public string Post([FromBody] string str)
+        {
+            return str;
+        }
+    }
+}
